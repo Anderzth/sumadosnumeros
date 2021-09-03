d71fe0b96677fa0ab3afacd8acab852ab5eabca8
Anderzth <90013195+Anderzth@users.noreply.github.com>
2021-09-02 19:33:13 -0500

    Add files via upload

---
+++ sumadosnumeros.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+printf "\n\e[1;92mCreado por Anderzth\n"
+figlet Anderzth
+
+read -p 'Ingresa numero 1= ' a
+read -p 'Ingresa numero 2= ' b
+
+let suma=$a+$b
+echo "La suma de $a + $b es= $suma"
+
+exit 0
+
